056dfa851bc2a05fbacf5b450f6e0876dea8e3e4
Messbah.a <Messbah.a@gmail.com>
2023-10-16 13:39:03 +0330

    rent updated - Messbah.a

---
+++ mft/model/da/rent_da.py
@@ -0,0 +1,46 @@
+def save():
+    pass
+
+
+def edit():
+    pass
+
+
+def find_all():
+    pass
+
+
+def find_by_code(code):
+    pass
+
+
+def find_by_sender_code(sender_code):
+    pass
+
+
+def find_by_renter_code(renter_code):
+    pass
+
+
+def find_by_stuff_code(stuff_code):
+    pass
+
+
+def find_by_rent_date_range(start_date, end_date):
+    pass
+
+
+def find_by_return_date_range(start_date, end_date):
+    pass
+
+
+def find_by_rent_price_range(start_price, end_price):
+    pass
+
+
+def find_by_information(information):
+    pass
+
+
+def find_by_rent_status(rent_status):
+    pass
