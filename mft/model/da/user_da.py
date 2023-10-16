a2aade52911251261c7da5cd2528bc4f3633c86e
Amir Ali <amiraliabasian23@gmail.com>
2023-10-16 14:07:33 +0330

    user_da updated amirali76

--- mft/model/da/user_da.py
+++ mft/model/da/user_da.py
@@ -1,8 +1,23 @@
-def save():
-    pass
+import mysql.connector
+def save(name,family,gender,age,username,password,email,role,state,city,address,phone,photo,status,score):
+    db=mysql.connector.connect(host = "localhost",user= "root",database="mft",port=3307)
+    cursor = db.cursor()
+    cursor.execute("insert into user_tbl(name,family,gender,age,username,password,email,role,state,city,address,phone,photo,status,score) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+                   [name,family,gender,age,username,password,email,role,state,city,address,phone,photo,status,score])
+    db.commit()
+    cursor.close()
+    db.close()
 
-def edit():
-    pass
+
+def edit(code,name,family,gender,age,username,password,email,role,state,city,address,phone,photo,status):
+    db = mysql.connector.connect(host="localhost", user="root", database="mft", port=3307)
+    cursor = db.cursor()
+    cursor.execute(
+        "update user_tbl set name=%s,family=%s,gender=%s,age=%s,username=%s,password=%s,email=%s,role,state,city,address,phone,photo,status ",
+        [name, family, gender, age, username, password, email, role, state, city, address, phone, photo, status,code])
+    db.commit()
+    cursor.close()
+    db.close()
 
 def activate(code):
     pass
