f7c43d05b2a14280e7f5c4830d068dee11c7b2f9
Messbah.a <Messbah.a@gmail.com>
2023-10-16 13:42:51 +0330

    stuff updated - Messbah.a

---
+++ mft/model/da/stuff_da.py
@@ -0,0 +1,32 @@
+def save():
+    pass
+
+def edit():
+    pass
+
+def remove(code):
+    pass
+
+def find_all():
+    pass
+
+def find_by_code(code):
+    pass
+
+def find_by_name(name):
+    pass
+
+def find_by_brand(brand):
+    pass
+
+def find_by_description(description):
+    pass
+
+def find_by_price_range(start_price, end_price):
+    pass
+
+def find_by_rent_price_range(start_price, end_price):
+    pass
+
+def find_by_condition(condition):
+    pass
