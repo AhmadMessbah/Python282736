5e2efca4c6dd5e1039cc91f3a2f9e442e54d54d9
amirhosein <afkhami.amirhosein.24@gmail.com>
2023-10-18 10:16:02 +0330

    user_da updated afkhami.amirhosein.24@gmail.com

--- mft/controller/user_controller.py
+++ mft/controller/user_controller.py
@@ -5,25 +5,26 @@ def valid_name(text):
     return bool (re.match("^[a-zA-Z\s]{2,30}$",text))
 
 def valid_username(text):
-    pass
+    return bool (re.match("^[a-zA-Z\_\-\d]{,20}$"))
 
 def valid_password(text):
-    pass
+    return bool (re.match("^[a-zA-Z]{,10}[\@]?[\_]?[\-]?[\d]{,6}$"))
 
 def valid_address(text):
-    pass
+    return bool(re.match("^[a-zA-z0-9\s\-],[\sآ-ی]{,100}$", text))
 
 def valid_email(text):
     pass
 
 def valid_phone(text):
-    pass
+    return bool (re.match("^09[\d]{9}$"))
 
 
 
 def save(name,family,gender,age,username,password,email,role,state,city,address,phone,photo,status,score):
     try:
-        if valid_name(name) and valid_name(family) and valid_name(state) and valid_name(city) and valid_name(photo):
+        if (valid_name(name) and valid_name(family) and valid_username(username) and valid_password(password) and valid_address(address) and valid_name(state) and valid_name(city)
+                and valid_phone(phone) and valid_name(photo)):
             save(name,family,gender,age,username,password,email,role,state,city,address,phone,photo,status,score)
             return True , "User Saved"
         else:
@@ -32,7 +33,8 @@ def save(name,family,gender,age,username,password,email,role,state,city,address,
         return False , str(e)
 def edit(name,family,gender,age,username,password,email,role,state,city,address,phone,photo,status,score):
     try:
-        if status == 1 and valid_name(name) and valid_name(family) and valid_name(state) and valid_name(city) and valid_name(photo):
+        if (status == 1 and valid_name(name) and valid_name(family) and valid_username(username) and valid_password(password) and valid_address(address) and valid_name(state) and valid_name(city)
+                and valid_phone(phone) and valid_name(photo)):
             edit(name,family,gender,age,username,password,email,role,state,city,address,phone,photo,status,score)
             return True , "User Edited"
         else:
@@ -46,8 +48,10 @@ def deactivate(code):
     pass
 
 
-def find_by_name_family(name,family):
-    pass
+def find_by_name_family(name,family,status):
+    try:
+        if status == 1 and valid_name(name) and valid_name(family):
+            find_by_name_family(name,family)
 
 def find_by_username(username):
     pass
