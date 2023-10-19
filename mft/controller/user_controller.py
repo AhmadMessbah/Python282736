9f0ba4b14538d6cae9104fa57eb1a9545192a8b4
parnia78 <parniamahmoudi78@gmail.com>
2023-10-19 21:01:00 +0330

    parnia

--- mft/controller/user_controller.py
+++ mft/controller/user_controller.py
@@ -1,48 +1,66 @@
 import re
 from mft.model.da.user_da import *
 
+
 def valid_name(text):
-    return bool (re.match("^[a-zA-Z\s]{2,30}$",text))
+    return bool(re.match("^[a-zA-Z\s]{2,30}$", text))
+
+
+def valid_age(number):
+    return bool(re.match("^[\d]+$", number))
+
 
 def valid_username(text):
-    return bool (re.match("^[a-zA-Z\_\-\d]{,20}$",text))
+    return bool(re.match("^[\w\s\-\*]{1,20}$", text))
+
 
 def valid_password(text):
-    return bool (re.match("^[a-zA-Z]{,10}[\@]?[\_]?[\-]?[\d]{,6}$",text))
+    return bool(re.match("(^[A-Z]{1,}[a-z]{1,}[\d]{1,}[@$!%?&*]{1,}$){8,}", text))
+
+
+def valid_email(text):
+    return bool(re.match("[\w\.]{1,50}@(outlook|gmail|yahoo).com", text, re.I))
+
 
 def valid_address(text):
     return bool(re.match("^[a-zA-z0-9\s\-],[\sآ-ی]{,100}$", text))
 
-def valid_email(text):
-    pass
 
 def valid_phone(text):
-    return bool (re.match("^09[\d]{9}$",text))
+    return bool(re.match("^09[\d]{9}$", text))
 
 
-
-def save(name,family,gender,age,username,password,email,role,state,city,address,phone,photo,status,score):
+def save(name, family, gender, age, username, password, email, role, state, city, address, phone, photo, status, score):
     try:
-        if (valid_name(name) and valid_name(family) and valid_username(username) and valid_password(password) and valid_address(address) and valid_name(state) and valid_name(city)
+        if (valid_name(name) and valid_name(family) and valid_username(username) and valid_password(
+                password) and valid_address(address) and valid_name(state) and valid_name(city)
                 and valid_phone(phone) and valid_name(photo)):
-            save_user(name,family,gender,age,username,password,email,role,state,city,address,phone,photo,status,score)
-            return True , "User Saved"
+            save_user(name, family, gender, age, username, password, email, role, state, city, address, phone, photo,
+                      status, score)
+            return True, "User Saved"
         else:
-            return False , "Save Error"
+            return False, "Save Error"
     except Exception as e:
-        return False , str(e)
-def edit(name,family,gender,age,username,password,email,role,state,city,address,phone,photo,status,score):
+        return False, str(e)
+
+
+def edit(name, family, gender, age, username, password, email, role, state, city, address, phone, photo, status, score):
     try:
-        if (status == 1 and valid_name(name) and valid_name(family) and valid_username(username) and valid_password(password) and valid_address(address) and valid_name(state) and valid_name(city)
+        if (status == 1 and valid_name(name) and valid_name(family) and valid_username(username) and valid_password(
+                password) and valid_address(address) and valid_name(state) and valid_name(city)
                 and valid_phone(phone) and valid_name(photo)):
-            edit_user(name,family,gender,age,username,password,email,role,state,city,address,phone,photo,status,score)
-            return True , "User Edited"
+            edit_user(name, family, gender, age, username, password, email, role, state, city, address, phone, photo,
+                      status, score)
+            return True, "User Edited"
         else:
-            return False , "Edit Error"
+            return False, "Edit Error"
     except Exception as e:
-        return False , str(e)
+        return False, str(e)
+
+
 def activate(code):
     pass
+
 
 def deactivate(code):
     pass
@@ -55,34 +73,45 @@ def find_all():
         return str(e)
 
 
+def find_by_name_family(name, family, status):
+    try:
+        if valid_name(name) and valid_name(family) and status == 1:
+            return find_by_name_family
+        else:
+            return False, "Invalid Data"
+    except Exception as e:
+        return False, str(e)
 
-def find_by_name_family(name,family,status):
-    pass
 
 def find_by_username(username):
     pass
 
+
 def find_by_role(role):
     pass
+
 
 def find_by_gender(gender):
     pass
 
+
 def find_by_score(code):
     pass
 
-def login(username,password):
+
+def login(username, password):
     try:
         if valid_username(username) and valid_password(password):
-            user = login_user(username,password)
+            user = login_user(username, password)
             if user:
-                return True , user
+                return True, user
             else:
-                return False , "Access Denied"
+                return False, "Access Denied"
         else:
-            return False , "Invalid Login Info"
+            return False, "Invalid Login Info"
     except Exception as e:
-        return False , str(e)
+        return False, str(e)
+
 
 def logout(username):
     pass
