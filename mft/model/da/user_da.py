938e1f85c148f7db01847b34c406aef1be37c60e
parnia78 <parniamahmoudi78@gmail.com>
2023-11-06 20:43:27 +0330

    added (return user) to save and edit

--- mft/model/da/user_da.py
+++ mft/model/da/user_da.py
@@ -27,22 +27,21 @@ class UserDa:
         self.disconnect(commit=commit)
         return result
 
-
-
-
-    def save_user(self,user):
+    def save_user(self, user):
         self.execute(
-        "insert into user_tbl (name,family,gender,age,username,password,email,role,state,city,address,phone,photo,status,score) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
-        [user.name, user.family, user.gender, user.age, user.username, user.password, user.email, user.role, user.state, user.city, user.address, user.phone, user.photo, user.status, user.score],
-        commit=True)
+            "insert into user_tbl (name,family,gender,age,username,password,email,role,state,city,address,phone,photo,status,score) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+            [user.name, user.family, user.gender, user.age, user.username, user.password, user.email, user.role,
+             user.state, user.city, user.address, user.phone, user.photo, user.status, user.score],
+            commit=True)
+        return user
 
-
-
-    def edit_user(self,user):
+    def edit_user(self, user):
         self.execute(
-        "update user_tbl set name=%s,family=%s,gender=%s,age=%s,username=%s,password=%s,email=%s,role=%s,state=%s,city=%s,address=%s,phone=%s,photo=%s,status=%s ",
-        [user.name, user.family, user.gender, user.age, user.username, user.password, user.email, user.role, user.state, user.city, user.address, user.phone, user.photo, user.status],
-        commit=True)
+            "update user_tbl set name=%s,family=%s,gender=%s,age=%s,username=%s,password=%s,email=%s,role=%s,state=%s,city=%s,address=%s,phone=%s,photo=%s,status=%s ",
+            [user.name, user.family, user.gender, user.age, user.username, user.password, user.email, user.role,
+             user.state, user.city, user.address, user.phone, user.photo, user.status],
+            commit=True)
+        return user
 
     def remove_user(self, code):
         self.execute('update user_tbl set deleted=1 where code=%s',
@@ -50,59 +49,47 @@ class UserDa:
                      commit=True)
         return code
 
-    def activate(self,code):
+    def activate(self, code):
         self.execute("update user_tbl set status = 1 where code = %s", [code],
-        commit=True)
+                     commit=True)
         return code
 
-
-# inactive
-    def deactivate(self,code):
+    # inactive
+    def deactivate(self, code):
         self.execute("update user_tbl set status = 0 where code = %s", [code],
-        commit=True)
+                     commit=True)
         return code
-
 
     def find_all_user(self):
-        return self.execute("select * from user_tbl",commit=True)
+        return self.execute("select * from user_tbl", commit=True)
 
+    def find_by_code(self, code):
+        return self.execute("select * from user_tbl where code=%s deleted =0", [code], commit=True)
 
-    def find_by_code(self,code):
-        return self.execute("select * from user_tbl where code=%s deleted =0", [code],commit=True)
+    def find_by_name(self, name):
+        return self.execute("select * from user_tbl where status=1 and name=%s  order by name", [name], commit=True)
 
+    def find_by_family(self, family):
+        return self.execute("select * from user_tbl where status=1 and family=%s  order by family", [family],
+                            commit=True)
 
+    def find_by_username(self, username):
+        return self.execute("select * from user_tbl where status=1 and username=%s order by username", [username],
+                            commit=True)
 
-    def find_by_name(self,name):
-        return self.execute("select * from user_tbl where status=1 and name=%s  order by name",[name],commit=True)
+    def find_by_role(self, role):
+        return self.execute("select * from user_tbl where status=1 and role=%s order by role", [role], commit=True)
 
-    def find_by_family(self,family):
-        return self.execute("select * from user_tbl where status=1 and family=%s  order by family",[family],commit=True)
+    def find_by_gender(self, gender):
+        return self.execute("select * from user_tbl where status=1 and gender=%s order by gender", [gender],
+                            commit=True)
 
-    def find_by_username(self,username):
-        return self.execute("select * from user_tbl where status=1 and username=%s order by username",[username],commit=True)
+    def find_by_score(self, score):
+        return self.execute("select * from user_tbl where status=1 and score=%s order by score", [score], commit=True)
 
+    def login_user(self, username, password):
+        return self.execute("select * from user_tbl where username = %s and password = %s and status = 1",
+                            [username, password], commit=True)
 
-
-    def find_by_role(self,role):
-        return self.execute("select * from user_tbl where status=1 and role=%s order by role",[role],commit=True)
-
-
-
-
-    def find_by_gender(self,gender):
-        return self.execute("select * from user_tbl where status=1 and gender=%s order by gender",[gender],commit=True)
-
-
-    def find_by_score(self,score):
-        return self.execute("select * from user_tbl where status=1 and score=%s order by score",[score],commit=True)
-
-
-
-    def login_user(self,username, password):
-        return self.execute("select * from user_tbl where username = %s and password = %s and status = 1", [username, password],commit=True)
-
-
-
-    def logout(self,username):
-        return self.execute("update user_tbl set status=0 where username=%s",[username],commit=True)
-
+    def logout(self, username):
+        return self.execute("update user_tbl set status=0 where username=%s", [username], commit=True)
