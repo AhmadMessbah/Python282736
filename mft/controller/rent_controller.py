00e277b2c96c37c81842b71c7b4348fb04152579
Student <sogolbasere@gmail.com>
2023-11-13 14:12:37 +0330

    Commit by rentGp

--- mft/controller/rent_controller.py
+++ mft/controller/rent_controller.py
@@ -1,49 +1,121 @@
 import re
-from rend_da import *
-
-def save():
-    pass
+from mft.model.da.rent_da import *
+from mft.model.entity.rent import Rent
 
 
-def edit():
-    pass
+class RentController:
+    def save(self, code, sender, renter, stuff, rent_date, return_date, stuff_status, rent_price, information):
+        try:
+            rent = Rent(code, sender, renter, stuff, rent_date, return_date, stuff_status, rent_price, information)
+            da = Rent(rent)
+            da.save(rent)
+            return True, rent
+        except Exception as e:
+            return False, str(e)
+
+    def edit(self, code, sender_code, renter_code, stuff_code, rent_date, return_date, stuff_status, rent_price,
+             information):
+        try:
+            rent = Rent(sender_code, renter_code, stuff_code, rent_date, return_date, stuff_status, rent_price,
+                        information)
+            da = Rent()
+            da.edit(rent)
+            return True, rent
+        except Exception as e:
+            return False, str(e)
+
+    def find_all(self):
+        try:
+            da = Rent()
+            return True, da.find_all()
+        except Exception as e:
+            return False, str(e)
+
+    def find_by_code(code):
+        try:
+            da = Rent()
+            return True, da.find_by_code()
+        except Exception as e:
+            return False, int(e)
+
+    def valid_code(code):
+        return re.match("^\ {1}$", code)
+
+    def find_by_sender_code(code):
+        try:
+            if code.valid_code(code) >= 0:
+                True, find_by_code()
+            else:
+                return False, "invalid Data"
+        except Exception as e:
+            False, int(e)
+
+    def valid_renter(renter_code):
+        return re.match("^\ {1}$")
+    def find_by_renter_code(renter_code):
+        try:
+            if renter_code.valid_renter(renter_code) >= 0:
+                True, find_by_code()
+            else:
+                return False, "invalid Data"
+        except Exception as e:
+            False, int(e)
+
+    def valid_stuff(stuff_code):
+        return re.match("^\ {1}$")
+    def find_by_stuff_code(stuff_code):
+        try:
+            if stuff_code.valid_renter(stuff_code) >= 0:
+                return True, find_by_code()
+            else:
+                return False, "invalid Data"
+        except Exception as e:
+            False, int(e)
+
+    #def date_validator(start_date, end_date):
+    #    re.match("^[2023/13/11,%s]" [start_date, end_date])
+    def find_by_rent_date_range(start_date, end_date):
+        try:
+            if start_date and end_date > 0:
+                return True, start_date(), end_date()
+            else:
+                return False, "invalid Date"
+        except Exception as e :
+            False, int(e)
 
 
-def find_all():
-    pass
+
+    def find_by_return_date_range(start_date, end_date):
+        try:
+            if start_date and end_date > 0:
+                return True, start_date(), end_date()
+            else:
+                return False, "invalid Date"
+        except Exception as e :
+            False, int(e)
+
+    def rent_price_validator(price):
+        return re.match("^[1000-10000]{1}",price)
+    def find_by_rent_price_range(start_price, end_price):
+        try:
+            if start_price and end_price > 0 :
+                return True, start_price(), end_price()
+            else:
+                return False, "Invalid Price"
+        except Exception as e :
+            False, int(e)
+
+    def valid_name(text):
+        return re.match('^[a-zA-Z\s]{500}$',text)
+    def find_by_information(information):
+        try :
+            if valid_name(information):
+                True , find_by_information()
+            else:
+                False, 'invalid information'
+        except Exception as e:
+            False , str(e)
 
 
-def find_by_code(code):
-    pass
-
-
-def find_by_sender_code(sender_code):
-    pass
-
-
-def find_by_renter_code(renter_code):
-    pass
-
-
-def find_by_stuff_code(stuff_code):
-    pass
-
-
-def find_by_rent_date_range(start_date, end_date):
-    pass
-
-
-def find_by_return_date_range(start_date, end_date):
-    pass
-
-
-def find_by_rent_price_range(start_price, end_price):
-    pass
-
-
-def find_by_information(information):
-    pass
-
-
-def find_by_rent_status(rent_status):
-    pass
+    def find_by_rent_status(rent_status):
+        pass
