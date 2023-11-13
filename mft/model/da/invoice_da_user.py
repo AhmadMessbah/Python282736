7353d6ff2920bbd725fd2239144fb4cde4289519
MohammadMehdiAhadi <mohammad.ahadi@gmail.com>
2023-11-13 13:56:13 +0330

    ghazal,mohammad,parnia,amirali,

--- mft/model/da/invoice_da_user.py
+++ mft/model/da/invoice_da_user.py
@@ -1,11 +1,31 @@
-from model.da.database import DatabaseManager
-from model.entity.user import *
-from model.entity.invoice import Invoice
+from mysqlx import *
+from sqlalchemy import *
+from sqlalchemy.orm import *
+
+from mft.model.da.database import DatabaseManager
+from mft.model.entity.invoice_user import Invoice
+
+
+class User:
+    __tablename__ = 'invoices'
+
+    id = Column(Integer, primary_key=True)
+
+    name = Column(String)
+    family = Column(String)
+    gender = Column(String)
+    role = Column(String)
+    score = Column(Integer)
+    username = Column(String)
+    password = Column(String)
+    user_id = Column(Integer, ForeignKey = "user.id")
+
+    user = relationship("User")
 
 
 class InvoiceDa(DatabaseManager):
     def find_by_customer_name(self,name):
         self.make_engine()
-        return self.session.query(Invoice).filter(Customer.name == name)
+        return self.session.query(Invoice).filter(User.name == name)
 
 
