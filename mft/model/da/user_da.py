f8c080625574fb9c9f4b46972dab897796d7db41
parnia78 <parniamahmoudi78@gmail.com>
2023-11-19 20:31:21 +0330

    changes on def

--- mft/model/da/user_da.py
+++ mft/model/da/user_da.py
@@ -15,43 +15,52 @@ class UserDa(DatabaseManager):
         self.make_engine()
         return self.session.query(User).filter(User.name.like(name))
 
-    def find_by_family(self, family):
+    def find_by_family(self, family, search_type=None):
+        match search_type:
+            case "contain":
+                family = "%" + family + "%"
+            case "start":
+                family = family + "%"
+            case "end":
+                family = "%" + family
+                self.make_engine()
+        return self.session.query(User).filter(User.family.like(family))
+
+    def find_by_gender(self, gender, search_type=None):
         pass
 
-    def find_by_gender(self, gender):
+    def find_by_age(self, age, search_type=None):
         pass
 
-    def find_by_age(self, age):
+    def find_by_username(self, username, search_type=None):
         pass
 
-    def find_by_username(self, username):
+    def find_by_password(self, password, search_type=None):
         pass
 
-    def find_by_password(self, password):
-        pass
-    def find_by_email(self, email):
+    def find_by_email(self, email, search_type=None):
         pass
 
-    def find_by_role(self, role):
+    def find_by_role(self, role, search_type=None):
         pass
 
-    def find_by_state(self, state):
+    def find_by_state(self, state, search_type=None):
         pass
 
-    def find_by_city(self, city):
+    def find_by_city(self, city, search_type=None):
         pass
 
-    def find_by_address(self, address):
+    def find_by_address(self, address, search_type=None):
         pass
 
-    def find_by_phone(self, phone):
+    def find_by_phone(self, phone, search_type=None):
         pass
 
-    def find_by_photo(self, photo):
+    def find_by_photo(self, photo, search_type=None):
         pass
 
-    def find_by_status(self, status):
+    def find_by_status(self, status, search_type=None):
         pass
 
-    def find_by_score(self, score):
+    def find_by_score(self, score, search_type=None):
         pass
